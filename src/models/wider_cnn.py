b05c1fc8b687fd233e8dd58a8f7d103f673e76e0
mmanuk7ann <manuk_manukyan@edu.aua.am>
2026-04-22 19:32:17 +0400

    Adding wider cnn

---
+++ src/models/wider_cnn.py
@@ -0,0 +1,44 @@
+import torch.nn as nn
+
+
+class WiderCNN(nn.Module):
+
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(3, 64, 3, padding=1)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.conv2 = nn.Conv2d(64, 64, 3, padding=1)
+        self.bn2 = nn.BatchNorm2d(64)
+        self.conv3 = nn.Conv2d(64, 128, 3, padding=1)
+        self.bn3 = nn.BatchNorm2d(128)
+        self.conv4 = nn.Conv2d(128, 128, 3, padding=1)
+        self.bn4 = nn.BatchNorm2d(128)
+        self.relu = nn.ReLU()
+        self.pool = nn.MaxPool2d(2)
+        self.flatten = nn.Flatten()
+        self.func1 = nn.Linear(128*8*8, 256)
+        self.func2 = nn.Linear(256, 10)
+        self.dropout = nn.Dropout(0.5)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.conv2(x)
+        x = self.bn2(x)
+        x = self.relu(x)
+        x = self.pool(x)
+        x = self.conv3(x)
+        x = self.bn3(x)
+        x = self.relu(x)
+        x = self.conv4(x)
+        x = self.bn4(x)
+        x = self.relu(x)
+        x = self.pool(x)
+        x = self.flatten(x)
+        x = self.func1(x)
+        x = self.relu(x)
+        x = self.dropout(x)
+        x = self.func2(x)
+
+        return x
