16716cf0dff1bf510f1c0dab7a9995fe635f1ccd
mmanuk7ann <manuk_manukyan@edu.aua.am>
2026-04-22 17:22:39 +0400

    Fix gitignore, add models package

---
+++ src/models/baseline_cnn.py
@@ -0,0 +1,34 @@
+import torch.nn as nn
+
+
+class BaselineCNN(nn.Module):
+
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(3, 32, 3, padding=1)
+        self.conv2 = nn.Conv2d(32, 32, 3, padding=1)
+        self.conv3 = nn.Conv2d(32, 64, 3, padding=1)
+        self.conv4 = nn.Conv2d(64, 64, 3, padding=1)
+        self.relu = nn.ReLU()
+        self.pool = nn.MaxPool2d(2)
+        self.flatten = nn.Flatten()
+        self.func1 = nn.Linear(64*8*8, 128)
+        self.func2 = nn.Linear(128, 10)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.relu(x)
+        x = self.conv2(x)
+        x = self.relu(x)
+        x = self.pool(x)
+        x = self.conv3(x)
+        x = self.relu(x)
+        x = self.conv4(x)
+        x = self.relu(x)
+        x = self.pool(x)
+        x = self.flatten(x)
+        x = self.func1(x)
+        x = self.relu(x)
+        x = self.func2(x)
+
+        return x
